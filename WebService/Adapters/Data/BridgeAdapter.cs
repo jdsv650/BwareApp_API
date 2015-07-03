a476806db3797ee28305746903083ebd951e951f
James D <jdsv650@yahoo.com>
2015-07-02 23:13:06 -0400

    Add  api/Bridge endpoint to get all bridges (unauthorized)

---
+++ WebService/Adapters/Data/BridgeAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Bware.Data.Model;
+using Bware.Data;
+using System.Web.Http;
+
+namespace WebService.Adapters.Data
+{
+    public class BridgeAdapter : Adapters.Interface.IBridgeAdapter
+    {
+        public IEnumerable<Bridge> getAllBridges()
+        {
+            var bridges = new List<Bridge>();
+            var db = new BwareContext();
+
+            bridges = db.Bridges.ToList();
+            return bridges;
+        }
+    }
+}
---
+++ WebService/Controllers/BridgeController.cs
@@ -0,0 +1,48 @@
+﻿using Bware.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebService.Controllers
+{
+    public class BridgeController : ApiController
+    {
+        Adapters.Interface.IBridgeAdapter _adapter;
+
+        public BridgeController()
+        {
+            _adapter = new Adapters.Data.BridgeAdapter();
+        }
+
+        // GET: api/Bridge
+        public IEnumerable<Bridge> Get()
+        {
+            var result = _adapter.getAllBridges();
+            return result;
+        }
+
+        // GET: api/Bridge/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Bridge
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Bridge/5Adapters
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Bridge/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
